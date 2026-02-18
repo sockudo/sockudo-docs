e5379931fbc008ba7e5254da266fe3bab1dec785
Diaconu Radu-Mihai <52667211+countradooku@users.noreply.github.com>
2026-02-18 16:38:31 +0200

    Add databuddy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,21 @@ export default defineNuxtConfig({
     head: {
       link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
       meta: [{ name: "theme-color", content: "#646cff" }],
+      script: [
+        {
+          src: "https://cdn.databuddy.cc/databuddy.js",
+          "data-client-id": "5aFTKbNqr8XkSznh3u3F3",
+          "data-track-hash-changes": "true",
+          "data-track-attributes": "true",
+          "data-track-outgoing-links": "true",
+          "data-track-interactions": "true",
+          "data-track-scroll-depth": "true",
+          "data-track-web-vitals": "true",
+          "data-track-errors": "true",
+          crossorigin: "anonymous",
+          async: true,
+        },
+      ],
     },
   },
 });
